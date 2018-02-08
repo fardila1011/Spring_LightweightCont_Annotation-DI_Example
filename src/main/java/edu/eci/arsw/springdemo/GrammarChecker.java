0c24c2ad030d4b3a8dec72c46f4102b12f8644cc
Fabian Ardila <fabianra97@gmail.com>
2018-02-08 17:59:10 -0500

    Adiciona anotaciones para injectar servicios al bean

--- src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
@@ -3,7 +3,7 @@ package edu.eci.arsw.springdemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+@Service
 public class GrammarChecker {
 
 	SpellChecker sc;
@@ -14,12 +14,12 @@ public class GrammarChecker {
 	public SpellChecker getSpellChecker() {
 		return sc;
 	}
-
+        
+        @Autowired
 	public void setSpellChecker(SpellChecker sc) {
 		this.sc = sc;
 	}
-
-
+        
 	public String check(String text){
 		
 		StringBuffer sb=new StringBuffer();
